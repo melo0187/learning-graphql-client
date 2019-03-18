1263653010edb3aeef6766f12528f909ac3ffd7b
Carmelo Scollo <carmelo.scollo@gmail.com>
2019-03-18 17:54:03 +0100

    Add Apollo with Query and Mutation
    This allows to peform defined queries and mutations at our API.
    I already added apollo-cache-persist for cache-first fetch, too.

---
+++ src/Users.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Query, Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import { ROOT_QUERY } from './App'
+
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`
+
+const Users = () =>
+  <Query query={ROOT_QUERY}>
+    {({ data, loading, refetch }) => loading ?
+      <p>Users are loading...</p> :
+      <UserList count={data.totalUsers} users={data.allUsers} refetch={refetch} />
+    }
+  </Query>
+
+const UserList = ({ count, users, refetch }) =>
+  <div>
+    <p>{count} Users</p>
+    <button onClick={() => refetch()}>Reload Users</button>
+    <Mutation mutation={ADD_FAKE_USERS_MUTATION}
+      variables={{ count: 1 }}
+      refetchQueries={[{ query: ROOT_QUERY }]} >
+      {addFakeUsers =>
+        <button onClick={addFakeUsers}>Add Fake Users</button>
+      }
+    </Mutation>
+    <ul>
+      {users.map(user =>
+        <UserListItem key={user.githubLogin}
+          name={user.name}
+          avatar={user.avatar} />
+      )}
+    </ul>
+  </div>
+
+const UserListItem = ({ name, avatar }) =>
+  <li>
+    <img src={avatar} width={48} height={48} alt="user's avatar" />
+    {name}
+  </li>
+
+export default Users
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { persistCache } from 'apollo-cache-persist'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  cache: cache,
+  uri: 'http://localhost:4000/graphql'
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const setupAndRender = async () => {
+  await persistCache({
+    cache,
+    storage: localStorage
+  })
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  )
+}
+
+setupAndRender()
