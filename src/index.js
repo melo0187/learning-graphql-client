da55e5fac30a23601ea8eabe93316b13539598ba
Carmelo Scollo <carmelo.scollo@gmail.com>
2019-03-20 11:08:43 +0100

    Change handling of restoring cache before render

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 import { ApolloProvider } from 'react-apollo'
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
 import { persistCache } from 'apollo-cache-persist'
 
 const cache = new InMemoryCache()
-const client = new ApolloClient({
-  cache: cache,
-  uri: 'http://localhost:4000/graphql',
-  request: operation => {
-    operation.setContext(context => ({
-      headers: {
-        ...context.headers,
-        authorization: localStorage.getItem('token')
-      }
-    }))
-  }
-})
 
-const setupAndRender = async () => {
-  await persistCache({
-    cache,
-    storage: localStorage
+persistCache({
+  cache,
+  storage: localStorage
+}).then(() => {
+  const client = new ApolloClient({
+    cache: cache,
+    uri: 'http://localhost:4000/graphql',
+    request: operation => {
+      operation.setContext(context => ({
+        headers: {
+          ...context.headers,
+          authorization: localStorage.getItem('token')
+        }
+      }))
+    }
   })
-  ReactDOM.render(
+
+  render(
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>,
     document.getElementById('root')
   )
-}
-
-setupAndRender()
+})
